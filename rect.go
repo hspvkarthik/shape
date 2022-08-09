e914172247e6e54504b8b42dd10518ffb0939d5e
Hari-Karthik <hspvkarthik@gmail.com>
2022-08-10 00:08:13 +0530

    Add rectangle perimeter definition

--- rect.go
+++ rect.go
@@ -11,3 +11,15 @@ func NewRectangle(length float64, width float64) Rectangle {
 	}
 	return Rectangle{length: length, width: width}
 }
+
+func (rect Rectangle) FindPerimeter() float64 {
+	if rect.length == rect.width {
+		return rect.length * 4
+	} else if rect.length == 1 {
+		return 2 + 2*rect.width
+	} else if rect.width == 1 {
+		return 2 + 2*rect.length
+	} else {
+		return 2 * (rect.length + rect.width)
+	}
+}
